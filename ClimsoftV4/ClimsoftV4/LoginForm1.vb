f6f40cfb164c6139302bd5ea2a878aea19a850ce
Ian Edwards <iedwards.pub@gmail.com>
2015-04-21 12:46:25 +0100

    Combined v4 code

---
+++ ClimsoftV4/ClimsoftV4/LoginForm1.vb
@@ -0,0 +1,24 @@
+﻿Public Class frm_login
+
+    ' TODO: Insert code to perform custom authentication using the provided username and password 
+    ' (See http://go.microsoft.com/fwlink/?LinkId=35339).  
+    ' The custom principal can then be attached to the current thread's principal as follows: 
+    '     My.User.CurrentPrincipal = CustomPrincipal
+    ' where CustomPrincipal is the IPrincipal implementation used to perform authentication. 
+    ' Subsequently, My.User will return identity information encapsulated in the CustomPrincipal object
+    ' such as the username, display name, etc.
+
+    Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
+
+        frm_main.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub LogoPictureBox_Click(sender As Object, e As EventArgs) Handles LogoPictureBox.Click
+
+    End Sub
+End Class
---
+++ ClimsoftV4/ClimsoftV4/formElement.vb
@@ -0,0 +1,15 @@
+﻿Public Class formElement
+
+    Private Sub ObselementBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles ObselementBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.ObselementBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.Mysql_climsoft_db_v4DataSet2)
+
+    End Sub
+
+    Private Sub formElement_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Mysql_climsoft_db_v4DataSet2.obselement' table. You can move, or remove it, as needed.
+        Me.ObselementTableAdapter.Fill(Me.Mysql_climsoft_db_v4DataSet2.obselement)
+
+    End Sub
+End Class
