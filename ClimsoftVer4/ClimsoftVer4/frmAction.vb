320180202f3a1e187bffa0227adf57b07f65cafd
isedwards <ian@myacorn.com>
2015-06-28 21:59:38 +0100

    Added multilingual support
    See example usage in frmAction.vb (note: you must first add
    language.resx and language.de.resx to the project)

--- ClimsoftVer4/ClimsoftVer4/frmAction.vb
+++ ClimsoftVer4/ClimsoftVer4/frmAction.vb
@@ -1,11 +1,16 @@
-﻿Public Class frmAction
+﻿Imports ClimsoftVer4.GlobalVariables
+
+Public Class frmAction
     Public HTMLHelp As New ClassHelp
     Private Sub cmdClose_Click(sender As Object, e As EventArgs) Handles cmdCancel.Click
         Me.Close()
     End Sub
 
     Private Sub frmAction_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        cmdOk.Text = lang("OK")
+        cmdCancel.Text = lang("Cancel")
+        cmdApply.Text = lang("Apply")
+        cmdHelp.Text = lang("Help")
     End Sub
 
     Private Sub cmdHelp_Click(sender As Object, e As EventArgs) Handles cmdHelp.Click
