95092330ab6dd896f6357e0ddc2fe32f2c631154
smachua <smmachua@yahoo.com>
2015-12-21 19:39:55 +0300

    samuel-update-21122015

--- ClimsoftVer4/ClimsoftVer4/frmDBUtilities.vb
+++ ClimsoftVer4/ClimsoftVer4/frmDBUtilities.vb
@@ -17,7 +17,12 @@
 Imports System.Windows.Forms
 
 Public Class frmDBUtilities
-
+    Dim da As MySql.Data.MySqlClient.MySqlDataAdapter
+    Dim ds As New DataSet
+    Dim sql As String
+    Dim conn As New MySql.Data.MySqlClient.MySqlConnection
+    Dim MyConnectionString As String
+    Dim cmd As New MySql.Data.MySqlClient.MySqlCommand
     Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
         Me.DialogResult = System.Windows.Forms.DialogResult.OK
         Me.Close()
@@ -42,5 +47,41 @@ Public Class frmDBUtilities
 
     Private Sub formDbUtilities_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub ImportToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ImportToolStripMenuItem.Click
+        Dim col As String
+        Dim itm As New ListViewItem
+
+        ListViewDbUtil.Visible = True
+        cmdUpload.Visible = True
+        conn.Close()
+        ListViewDbUtil.Clear()
+        ListViewDbUtil.Columns.Clear()
+
+        'ListViewDbUtil.Columns.Add("Form Name", 100, HorizontalAlignment.Left)
+        ListViewDbUtil.Columns.Add("Select Form to upload", 600, HorizontalAlignment.Left)
+        Try
+            MyConnectionString = frmLogin.txtusrpwd.Text
+            conn.ConnectionString = MyConnectionString
+            conn.Open()
+
+            Sql = "SELECT selected,form_name, description FROM data_forms;"
+            da = New MySql.Data.MySqlClient.MySqlDataAdapter(Sql, conn)
+            ds.Clear()
+            da.Fill(ds, "data_forms")
+
+            For i = 0 To ds.Tables("data_forms").Rows.Count - 1
+                'col(0) = ds.Tables("data_forms").Rows(i).Item(1)
+                If ds.Tables("data_forms").Rows(i).Item(0) = 1 Then
+                    col = ds.Tables("data_forms").Rows(i).Item(2)
+                    itm = New ListViewItem(col)
+                    ListViewDbUtil.Items.Add(itm)
+                End If
+                'If ds.Tables("data_forms").Rows(i).Item(0) = 1 Then lstvForms.Items(i).Checked = True
+            Next
+        Catch ex As MySql.Data.MySqlClient.MySqlException
+            MessageBox.Show(ex.Message)
+        End Try
     End Sub
 End Class
