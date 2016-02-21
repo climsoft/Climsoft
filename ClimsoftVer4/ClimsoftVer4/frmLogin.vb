0eef97b04d4953c792883274a684c98b7d4e1b6a
Ian Edwards <ian@myacorn.com>
2016-02-21 16:16:17 +0000

    Added GPL 3 licence information

--- ClimsoftVer4/ClimsoftVer4/frmLogin.vb
+++ ClimsoftVer4/ClimsoftVer4/frmLogin.vb
@@ -1,5 +1,5 @@
 ﻿' CLIMSOFT - Climate Database Management System
-' Copyright (C) 2015
+' Copyright (C) 2016
 '
 ' This program is free software: you can redistribute it and/or modify
 ' it under the terms of the GNU General Public License as published by
