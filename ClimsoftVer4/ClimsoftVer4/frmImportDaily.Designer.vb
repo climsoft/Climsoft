5daa04f6b283736fe6baf71d03e6296b615a8d86
smachua <smmachua@yahoo.com>
2017-06-13 16:51:49 +0300

    Samuel_update_13062017_1
    Latest update on beta2 branch

--- ClimsoftVer4/ClimsoftVer4/frmImportDaily.Designer.vb
+++ ClimsoftVer4/ClimsoftVer4/frmImportDaily.Designer.vb
@@ -53,6 +53,7 @@ Partial Class frmImportDaily
         Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
         Me.cmdClose = New System.Windows.Forms.Button()
         Me.cmdHelp = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.pnlHeaders.SuspendLayout()
         Me.SuspendLayout()
@@ -68,9 +69,9 @@ Partial Class frmImportDaily
         '
         'cmdOpenFile
         '
-        Me.cmdOpenFile.Location = New System.Drawing.Point(468, 0)
+        Me.cmdOpenFile.Location = New System.Drawing.Point(547, 0)
         Me.cmdOpenFile.Name = "cmdOpenFile"
-        Me.cmdOpenFile.Size = New System.Drawing.Size(91, 23)
+        Me.cmdOpenFile.Size = New System.Drawing.Size(64, 23)
         Me.cmdOpenFile.TabIndex = 1
         Me.cmdOpenFile.Text = "Select File"
         Me.cmdOpenFile.UseVisualStyleBackColor = True
@@ -81,7 +82,7 @@ Partial Class frmImportDaily
         '
         'txtImportFile
         '
-        Me.txtImportFile.Location = New System.Drawing.Point(4, 3)
+        Me.txtImportFile.Location = New System.Drawing.Point(84, 1)
         Me.txtImportFile.Name = "txtImportFile"
         Me.txtImportFile.Size = New System.Drawing.Size(464, 20)
         Me.txtImportFile.TabIndex = 2
@@ -333,11 +334,21 @@ Partial Class frmImportDaily
         Me.cmdHelp.Text = "Help"
         Me.cmdHelp.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 29
+        Me.Label1.Text = "Import File"
+        '
         'frmImportDaily
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(970, 552)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.cmdHelp)
         Me.Controls.Add(Me.cmdClose)
         Me.Controls.Add(Me.pnlHeaders)
@@ -398,4 +409,5 @@ Partial Class frmImportDaily
     Friend WithEvents ToolStripLabel1 As System.Windows.Forms.ToolStripLabel
     Friend WithEvents cmdClose As System.Windows.Forms.Button
     Friend WithEvents cmdHelp As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 End Class
