adf3b1ffad043095a32e4cb5f810c1ea3e210f2d
Danny Parsons <danny@aims.ac.za>
2018-01-17 18:30:27 +0000

    added user controls and classes for data display and entry

---
+++ ClimsoftVer4/ClimsoftVer4/clsDataCall.vb
@@ -0,0 +1,9 @@
+﻿Public Class DataCall
+    Private strTable As String
+    Private lstFields As List(Of String)
+    Private clsFilter As TableFilter
+
+    Public Function GetValues() As List(Of String)
+
+    End Function
+End Class
---
+++ ClimsoftVer4/ClimsoftVer4/ucrBaseDataLink.vb
@@ -0,0 +1,33 @@
+﻿' CLIMSOFT - Climate Database Management System
+' Copyright (C) 2017
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Public Class ucrBaseDataLink
+    Private clsDataDefinition As New DataCall
+
+    ' ucrBaseDataLink is a base control for a control to connect to the database
+    ' Infomation about how the control connects to the database will be here
+    ' Including: reading/writing, which tables/fields/records to connect to
+
+    ' Inherited controls:
+    ' textbox, nud, checkbox, combobox, listbox etc.
+
+    ' Inherited from these can be controls for common fields:
+    ' station selector, element selector, tmax entry etc.
+
+    ' 
+    Public Sub SetField()
+
+    End Sub
+End Class
