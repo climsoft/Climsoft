885bad35f7a0ebd2f62c32de1f4819f8322159f2
shadrack kibet <shadrackkibet@outlook.com>
2018-01-29 18:33:44 +0300

    added filter subdialog

---
+++ ClimsoftVer4/ClimsoftVer4/sdgFilter.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class sdgFilter
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.tbCombined = New System.Windows.Forms.TabPage()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnRigthFilter = New System.Windows.Forms.Button()
+        Me.btnLeftFilter = New System.Windows.Forms.Button()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.tbCombined.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(3, 28)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(130, 28)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(50, 21)
+        Me.ComboBox2.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(186, 28)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(59, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.tbCombined)
+        Me.TabControl1.Location = New System.Drawing.Point(3, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(269, 284)
+        Me.TabControl1.TabIndex = 3
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.CheckedListBox1)
+        Me.TabPage1.Controls.Add(Me.TextBox1)
+        Me.TabPage1.Controls.Add(Me.ComboBox1)
+        Me.TabPage1.Controls.Add(Me.ComboBox2)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(261, 258)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Simple"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(190, 12)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(37, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Value:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(132, 12)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Operator:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Element:"
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Location = New System.Drawing.Point(122, 65)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(128, 184)
+        Me.CheckedListBox1.TabIndex = 3
+        '
+        'tbCombined
+        '
+        Me.tbCombined.Controls.Add(Me.Label4)
+        Me.tbCombined.Controls.Add(Me.btnRigthFilter)
+        Me.tbCombined.Controls.Add(Me.btnLeftFilter)
+        Me.tbCombined.Controls.Add(Me.ComboBox3)
+        Me.tbCombined.Location = New System.Drawing.Point(4, 22)
+        Me.tbCombined.Name = "tbCombined"
+        Me.tbCombined.Padding = New System.Windows.Forms.Padding(3)
+        Me.tbCombined.Size = New System.Drawing.Size(261, 258)
+        Me.tbCombined.TabIndex = 1
+        Me.tbCombined.Text = "Combined"
+        Me.tbCombined.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(87, 26)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Operator:"
+        '
+        'btnRigthFilter
+        '
+        Me.btnRigthFilter.Location = New System.Drawing.Point(144, 42)
+        Me.btnRigthFilter.Name = "btnRigthFilter"
+        Me.btnRigthFilter.Size = New System.Drawing.Size(75, 23)
+        Me.btnRigthFilter.TabIndex = 4
+        Me.btnRigthFilter.Text = "Right Filter"
+        Me.btnRigthFilter.UseVisualStyleBackColor = True
+        '
+        'btnLeftFilter
+        '
+        Me.btnLeftFilter.Location = New System.Drawing.Point(7, 42)
+        Me.btnLeftFilter.Name = "btnLeftFilter"
+        Me.btnLeftFilter.Size = New System.Drawing.Size(75, 23)
+        Me.btnLeftFilter.TabIndex = 3
+        Me.btnLeftFilter.Text = "Left Filter"
+        Me.btnLeftFilter.UseVisualStyleBackColor = True
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(88, 42)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(50, 21)
+        Me.ComboBox3.TabIndex = 2
+        '
+        'sdgFilter
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 326)
+        Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "sdgFilter"
+        Me.Text = "Filter"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.tbCombined.ResumeLayout(False)
+        Me.tbCombined.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents tbCombined As TabPage
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents btnRigthFilter As Button
+    Friend WithEvents btnLeftFilter As Button
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ ClimsoftVer4/ClimsoftVer4/sdgFilter.vb
@@ -0,0 +1,13 @@
+﻿Public Class sdgFilter
+    Private Sub btnLeftFilter_Click(sender As Object, e As EventArgs) Handles btnLeftFilter.Click
+
+    End Sub
+
+    Private Sub btnRigthFilter_Click(sender As Object, e As EventArgs) Handles btnRigthFilter.Click
+
+    End Sub
+
+    Private Sub sdgFilter_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
