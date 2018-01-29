1893f86bd7c0ddd19f093c10d9c5d2d4e1c37180
Danny Parsons <danny@aims.ac.za>
2018-01-29 15:06:49 +0300

    added GetDataSet in DataCall

--- ClimsoftVer4/ClimsoftVer4/ucrStationSelector.vb
+++ ClimsoftVer4/ClimsoftVer4/ucrStationSelector.vb
@@ -10,15 +10,16 @@
     Private Sub PopulateStationList()
         ' Example of defining a filter for the data call
         'clsDataDefinition.SetFilter(strStationID, "==", Chr(34) & "67774010" & Chr(34))
-        objStations = clsDataDefinition.GetDataTable()
-        dtbStations = New DataTable()
-        dtbStations.Columns.Add(strStationName, GetType(String))
-        dtbStations.Columns.Add(strStationID, GetType(String))
-        dtbStations.Columns.Add(strIDsAndStations, GetType(String))
+        dtbStations = clsDataDefinition.GetDataTable()
+        'objStations = clsDataDefinition.GetDataTable()
+        'dtbStations = New DataTable()
+        'dtbStations.Columns.Add(strStationName, GetType(String))
+        'dtbStations.Columns.Add(strStationID, GetType(String))
+        'dtbStations.Columns.Add(strIDsAndStations, GetType(String))
 
-        For Each stnItem As station In objStations
-            dtbStations.Rows.Add(stnItem.stationName, stnItem.stationId, stnItem.stationId & " " & stnItem.stationName)
-        Next
+        'For Each stnItem As station In objStations
+        '    dtbStations.Rows.Add(stnItem.stationName, stnItem.stationId, stnItem.stationId & " " & stnItem.stationName)
+        'Next
         cboValues.DataSource = dtbStations
         ' May need ValueMember to be different in different instances e.g. if station name is needed as return value
         cboValues.ValueMember = strStationID
@@ -57,11 +58,15 @@
     End Sub
 
     Private Sub ucrStationSelector_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim d As New Dictionary(Of String, List(Of String))
         If bFirstLoad Then
             'InitialiseStationDataTable()
             'SortByStationName()
             SetTable(strStationsTableName)
-            SetFields(New List(Of String)({strStationID, strStationName}))
+            d.Add("Stations", New List(Of String)({strStationName}))
+            d.Add("IDs", New List(Of String)({strStationID}))
+            d.Add("IDs and Stations", New List(Of String)({strStationName, strStationID}))
+            SetFields(d)
             PopulateStationList()
             bFirstLoad = False
         End If
