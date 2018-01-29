979796e2e75764e7378b6966787d42982b6eec60
Danny <danny@aims.ac.za>
2018-01-29 18:00:36 +0300

    fixed error adding user controls to forms and corrected data call definition on station/element selectors

--- ClimsoftVer4/ClimsoftVer4/ucrElemSelector.vb
+++ ClimsoftVer4/ClimsoftVer4/ucrElemSelector.vb
@@ -1,8 +1,8 @@
 ﻿Public Class ucrElemSelector
     Private bFirstLoad As Boolean = True
-    Private strElementTableName As String = "obsElem"
+    Private strElementTableName As String = "obselements"
     Private strElementName As String = "elementName"
-    Private strelementID As String = "elementId"
+    Private strElementID As String = "elementId"
     Private strIDsAndElements As String = "ids_elements"
     Private objElements As New Object
     Private dtbElements As New DataTable
@@ -10,20 +10,24 @@
     Private Sub PopulateElementList()
         ' Example of defining a filter for the data call
         'clsDataDefinition.SetFilter(strelementID, "==", Chr(34) & "67774010" & Chr(34))
-        objElements = clsDataDefinition.GetDataTable()
-        dtbElements = New DataTable()
-        dtbElements.Columns.Add(strElementName, GetType(String))
-        dtbElements.Columns.Add(strelementID, GetType(String))
-        dtbElements.Columns.Add(strIDsAndElements, GetType(String))
+        dtbElements = clsDataDefinition.GetDataTable()
+        'dtbElements = New DataTable()
+        'dtbElements.Columns.Add(strElementName, GetType(String))
+        'dtbElements.Columns.Add(strelementID, GetType(String))
+        'dtbElements.Columns.Add(strIDsAndElements, GetType(String))
 
-        For Each elemItem As obselement In objElements
-            dtbElements.Rows.Add(elemItem.elementName, elemItem.elementId, elemItem.elementId & " " & elemItem.elementName)
-        Next
-        cboValues.DataSource = dtbElements
-        ' May need ValueMember to be different in different instances e.g. if station name is needed as return value
-        cboValues.ValueMember = strelementID
-        If bFirstLoad Then
-            SetViewTypeAsElements()
+        'For Each elemItem As obselement In objElements
+        '    dtbElements.Rows.Add(elemItem.elementName, elemItem.elementId, elemItem.elementId & " " & elemItem.elementName)
+        'Next
+        If dtbElements.Rows.Count > 0 Then
+            cboValues.DataSource = dtbElements
+            ' May need ValueMember to be different in different instances e.g. if station name is needed as return value
+            cboValues.ValueMember = strElementID
+            If bFirstLoad Then
+                SetViewTypeAsElements()
+            End If
+        Else
+            cboValues.DataSource = Nothing
         End If
     End Sub
 
@@ -35,9 +39,9 @@
             Case strElementName
                 '        tsmStationNames.Checked = True
                 cboValues.DisplayMember = strElementName
-            Case strelementID
+            Case strElementID
                 '        tsmIDs.Checked = True
-                cboValues.DisplayMember = strelementID
+                cboValues.DisplayMember = strElementID
             Case strIDsAndElements
                 '        tsmIDsAndStations.Checked = True
                 cboValues.DisplayMember = strIDsAndElements
@@ -49,7 +53,7 @@
     End Sub
 
     Public Sub SetViewTypeAsIDs()
-        SetViewType(strelementID)
+        SetViewType(strElementID)
     End Sub
 
     Public Sub SetViewTypeAsIDsAndElements()
@@ -57,11 +61,16 @@
     End Sub
 
     Private Sub ucrElemSelector_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim d As New Dictionary(Of String, List(Of String))
+
         If bFirstLoad Then
             'InitialiseStationDataTable()
             'SortByStationName()
             SetTable(strElementTableName)
-            SetFields(New List(Of String)({strelementID, strElementName}))
+            d.Add(strElementName, New List(Of String)({strElementName}))
+            d.Add(strElementID, New List(Of String)({strElementID}))
+            d.Add(strIDsAndElements, New List(Of String)({strElementID, strElementName}))
+            SetFields(d)
             PopulateElementList()
             bFirstLoad = False
         End If
