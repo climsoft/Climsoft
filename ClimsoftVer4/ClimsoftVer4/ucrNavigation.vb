55bc4e3b1c805216e7cd5020080731ae3b1f9116
patowhiz <conlooptechnologies@gmail.com>
2018-02-20 21:05:32 +0300

    Tyding up of code

--- ClimsoftVer4/ClimsoftVer4/ucrNavigation.vb
+++ ClimsoftVer4/ClimsoftVer4/ucrNavigation.vb
@@ -8,12 +8,6 @@
         currRow = 0
         maxRows = dtbRecords.Rows.Count
         displayRecordNumber()
-        If maxRows > 0 Then
-
-        Else
-
-        End If
-
     End Sub
 
     'Not sure whether it's necessary to override this
@@ -56,43 +50,28 @@
     Private Sub btnMoveFirst_Click(sender As Object, e As EventArgs) Handles btnMoveFirst.Click
         'In order to move to move to the first record the record index is set to zero.
         currRow = 0
-        'Call subroutine for record navigation
-        'navigateRecords()
-
         OnevtValueChanged(sender, e)
+
     End Sub
 
     Private Sub btnMovePrevious_Click(sender As Object, e As EventArgs) Handles btnMovePrevious.Click
-        'Instantiate the "dataEntryGlobalRoutines" in order to access its methods.
-        Dim noPreviousRec As New dataEntryGlobalRoutines
 
         If currRow > 0 Then
             currRow = currRow - 1
-            'navigateRecords()
             OnevtValueChanged(sender, e)
         Else
-            'If the record Index is equal to zero an error message must be displayed to show that there is no more previous record.
-            'The message to be displayed is provided by a subroutine in the "dataEntryCommonRoutines" class hence the need to
-            'instantiate the "dataEntryCommonRoutines" class in the Declaration above.
-            'noPreviousRec.messageBoxNoPreviousRecord()
-            MsgBox("First Row")
+            MsgBox("No more previous record!", MsgBoxStyle.Exclamation)
         End If
+
     End Sub
 
     Private Sub btnMoveNext_Click(sender As Object, e As EventArgs) Handles btnMoveNext.Click
-        'Instantiate the "dataEntryGlobalRoutines" in order to access its methods.
-        ' Dim noNextRec As New dataEntryGlobalRoutines
+
         If currRow < (maxRows - 1) Then
             currRow = currRow + 1
-            'Call subroutine for record navigation
-            'navigateRecords()
             OnevtValueChanged(sender, e)
         Else
-            'If the record Index is equal to maximum number of rows minus one, an error message must be displayed to show that
-            'there is no more next record.The message to be displayed is provided by a subroutine in the "dataEntryCommonRoutines" class
-            'hence the need to instantiate the "dataEntryCommonRoutines" class in the Declaration above.
-            'noNextRec.messageBoxNoNextRecord()
-            MsgBox("Last Row")
+            MsgBox("No more next record!", MsgBoxStyle.Exclamation)
         End If
 
     End Sub
@@ -100,9 +79,8 @@
     Private Sub btnMoveLast_Click(sender As Object, e As EventArgs) Handles btnMoveLast.Click
         'In order to move to move to the last record the record index is set to the maximum number of records minus one.
         currRow = maxRows - 1
-        'Call subroutine for record navigation
-        'navigateRecords()
         OnevtValueChanged(sender, e)
+
     End Sub
 
 
