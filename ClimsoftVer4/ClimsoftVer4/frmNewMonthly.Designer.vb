ede3269f797261c406232798d45ce1d6fe4f0259
shadrack kibet <shadrackkibet@outlook.com>
2018-02-21 21:20:00 +0300

    designed ucrMonthlyData and frmNewMonthlyData

---
+++ ClimsoftVer4/ClimsoftVer4/frmNewMonthly.Designer.vb
@@ -0,0 +1,305 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmNewMonthly
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim lblYear As System.Windows.Forms.Label
+        Me.lblElement = New System.Windows.Forms.Label()
+        Me.lblStation = New System.Windows.Forms.Label()
+        Me.ucrStationSelector = New ClimsoftVer4.ucrStationSelector()
+        Me.ucrElementSelector = New ClimsoftVer4.ucrElementSelector()
+        Me.ucrYearSelector = New ClimsoftVer4.ucrYearSelector()
+        Me.btnView = New System.Windows.Forms.Button()
+        Me.btnUpload = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtSequencer = New System.Windows.Forms.TextBox()
+        Me.btnHelp = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnCommit = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnAddNew = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnMovePrevious = New System.Windows.Forms.Button()
+        Me.btnMoveFirst = New System.Windows.Forms.Button()
+        Me.btnMoveLast = New System.Windows.Forms.Button()
+        Me.recNumberTextBox = New System.Windows.Forms.TextBox()
+        Me.btnMoveNext = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        lblYear = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblYear
+        '
+        lblYear.AutoSize = True
+        lblYear.Location = New System.Drawing.Point(514, 27)
+        lblYear.Name = "lblYear"
+        lblYear.Size = New System.Drawing.Size(32, 13)
+        lblYear.TabIndex = 494
+        lblYear.Text = "Year:"
+        '
+        'lblElement
+        '
+        Me.lblElement.AutoSize = True
+        Me.lblElement.Location = New System.Drawing.Point(280, 27)
+        Me.lblElement.Name = "lblElement"
+        Me.lblElement.Size = New System.Drawing.Size(48, 13)
+        Me.lblElement.TabIndex = 496
+        Me.lblElement.Text = "Element:"
+        '
+        'lblStation
+        '
+        Me.lblStation.AutoSize = True
+        Me.lblStation.Location = New System.Drawing.Point(44, 27)
+        Me.lblStation.Name = "lblStation"
+        Me.lblStation.Size = New System.Drawing.Size(43, 13)
+        Me.lblStation.TabIndex = 495
+        Me.lblStation.Text = "Station:"
+        '
+        'ucrStationSelector
+        '
+        Me.ucrStationSelector.Location = New System.Drawing.Point(90, 22)
+        Me.ucrStationSelector.Name = "ucrStationSelector"
+        Me.ucrStationSelector.Size = New System.Drawing.Size(187, 24)
+        Me.ucrStationSelector.TabIndex = 497
+        '
+        'ucrElementSelector
+        '
+        Me.ucrElementSelector.Location = New System.Drawing.Point(331, 22)
+        Me.ucrElementSelector.Name = "ucrElementSelector"
+        Me.ucrElementSelector.Size = New System.Drawing.Size(178, 21)
+        Me.ucrElementSelector.TabIndex = 498
+        '
+        'ucrYearSelector
+        '
+        Me.ucrYearSelector.Location = New System.Drawing.Point(551, 22)
+        Me.ucrYearSelector.Name = "ucrYearSelector"
+        Me.ucrYearSelector.Size = New System.Drawing.Size(85, 25)
+        Me.ucrYearSelector.TabIndex = 499
+        '
+        'btnView
+        '
+        Me.btnView.Location = New System.Drawing.Point(424, 410)
+        Me.btnView.Name = "btnView"
+        Me.btnView.Size = New System.Drawing.Size(75, 23)
+        Me.btnView.TabIndex = 677
+        Me.btnView.Text = "View"
+        Me.btnView.UseVisualStyleBackColor = True
+        '
+        'btnUpload
+        '
+        Me.btnUpload.BackColor = System.Drawing.Color.Lime
+        Me.btnUpload.Location = New System.Drawing.Point(586, 443)
+        Me.btnUpload.Name = "btnUpload"
+        Me.btnUpload.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpload.TabIndex = 676
+        Me.btnUpload.Text = "Upload"
+        Me.btnUpload.UseVisualStyleBackColor = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(183, 453)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(59, 13)
+        Me.Label5.TabIndex = 675
+        Me.Label5.Text = "Sequencer"
+        '
+        'txtSequencer
+        '
+        Me.txtSequencer.Location = New System.Drawing.Point(248, 450)
+        Me.txtSequencer.Name = "txtSequencer"
+        Me.txtSequencer.Size = New System.Drawing.Size(200, 20)
+        Me.txtSequencer.TabIndex = 674
+        Me.txtSequencer.Text = "seq_year"
+        '
+        'btnHelp
+        '
+        Me.btnHelp.Location = New System.Drawing.Point(586, 410)
+        Me.btnHelp.Name = "btnHelp"
+        Me.btnHelp.Size = New System.Drawing.Size(75, 23)
+        Me.btnHelp.TabIndex = 668
+        Me.btnHelp.Text = "Help"
+        Me.btnHelp.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Enabled = False
+        Me.btnClear.Location = New System.Drawing.Point(343, 410)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 666
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnCommit
+        '
+        Me.btnCommit.Enabled = False
+        Me.btnCommit.Location = New System.Drawing.Point(100, 410)
+        Me.btnCommit.Name = "btnCommit"
+        Me.btnCommit.Size = New System.Drawing.Size(75, 23)
+        Me.btnCommit.TabIndex = 662
+        Me.btnCommit.Text = "Save"
+        Me.btnCommit.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(262, 410)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 665
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnAddNew
+        '
+        Me.btnAddNew.Location = New System.Drawing.Point(19, 410)
+        Me.btnAddNew.Name = "btnAddNew"
+        Me.btnAddNew.Size = New System.Drawing.Size(75, 23)
+        Me.btnAddNew.TabIndex = 663
+        Me.btnAddNew.Text = "Add New"
+        Me.btnAddNew.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(181, 410)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpdate.TabIndex = 664
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnMovePrevious
+        '
+        Me.btnMovePrevious.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMovePrevious.Location = New System.Drawing.Point(220, 380)
+        Me.btnMovePrevious.Name = "btnMovePrevious"
+        Me.btnMovePrevious.Size = New System.Drawing.Size(46, 23)
+        Me.btnMovePrevious.TabIndex = 673
+        Me.btnMovePrevious.Text = "<<"
+        Me.btnMovePrevious.UseVisualStyleBackColor = True
+        '
+        'btnMoveFirst
+        '
+        Me.btnMoveFirst.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMoveFirst.Location = New System.Drawing.Point(173, 380)
+        Me.btnMoveFirst.Name = "btnMoveFirst"
+        Me.btnMoveFirst.Size = New System.Drawing.Size(41, 23)
+        Me.btnMoveFirst.TabIndex = 672
+        Me.btnMoveFirst.Text = "|<<"
+        Me.btnMoveFirst.UseVisualStyleBackColor = True
+        '
+        'btnMoveLast
+        '
+        Me.btnMoveLast.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMoveLast.Location = New System.Drawing.Point(463, 380)
+        Me.btnMoveLast.Name = "btnMoveLast"
+        Me.btnMoveLast.Size = New System.Drawing.Size(41, 23)
+        Me.btnMoveLast.TabIndex = 671
+        Me.btnMoveLast.Text = ">>|"
+        Me.btnMoveLast.UseVisualStyleBackColor = True
+        '
+        'recNumberTextBox
+        '
+        Me.recNumberTextBox.Location = New System.Drawing.Point(272, 382)
+        Me.recNumberTextBox.Name = "recNumberTextBox"
+        Me.recNumberTextBox.Size = New System.Drawing.Size(141, 20)
+        Me.recNumberTextBox.TabIndex = 670
+        '
+        'btnMoveNext
+        '
+        Me.btnMoveNext.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMoveNext.Location = New System.Drawing.Point(419, 380)
+        Me.btnMoveNext.Name = "btnMoveNext"
+        Me.btnMoveNext.Size = New System.Drawing.Size(38, 23)
+        Me.btnMoveNext.TabIndex = 669
+        Me.btnMoveNext.Text = ">>"
+        Me.btnMoveNext.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(505, 410)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 667
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'frmNewMonthly
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(681, 476)
+        Me.Controls.Add(Me.btnView)
+        Me.Controls.Add(Me.btnUpload)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtSequencer)
+        Me.Controls.Add(Me.btnHelp)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCommit)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnAddNew)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnMovePrevious)
+        Me.Controls.Add(Me.btnMoveFirst)
+        Me.Controls.Add(Me.btnMoveLast)
+        Me.Controls.Add(Me.recNumberTextBox)
+        Me.Controls.Add(Me.btnMoveNext)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.ucrYearSelector)
+        Me.Controls.Add(Me.ucrElementSelector)
+        Me.Controls.Add(Me.ucrStationSelector)
+        Me.Controls.Add(Me.lblElement)
+        Me.Controls.Add(Me.lblStation)
+        Me.Controls.Add(lblYear)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmNewMonthly"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Monthly Data"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblElement As Label
+    Friend WithEvents lblStation As Label
+    Friend WithEvents ucrStationSelector As ucrStationSelector
+    Friend WithEvents ucrElementSelector As ucrElementSelector
+    Friend WithEvents ucrYearSelector As ucrYearSelector
+    Friend WithEvents btnView As Button
+    Friend WithEvents btnUpload As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtSequencer As TextBox
+    Friend WithEvents btnHelp As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnCommit As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnAddNew As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnMovePrevious As Button
+    Friend WithEvents btnMoveFirst As Button
+    Friend WithEvents btnMoveLast As Button
+    Friend WithEvents recNumberTextBox As TextBox
+    Friend WithEvents btnMoveNext As Button
+    Friend WithEvents btnClose As Button
+End Class
---
+++ ClimsoftVer4/ClimsoftVer4/ucrMonthlydata.Designer.vb
@@ -0,0 +1,360 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ucrMonthlydata
+    Inherits ClimsoftVer4.ucrBaseDataLink
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.UcrValueFlagPeriod1 = New ClimsoftVer4.ucrValueFlagPeriod()
+        Me.UcrValueFlagPeriod2 = New ClimsoftVer4.ucrValueFlagPeriod()
+        Me.UcrValueFlagPeriod3 = New ClimsoftVer4.ucrValueFlagPeriod()
+        Me.UcrValueFlagPeriod4 = New ClimsoftVer4.ucrValueFlagPeriod()
+        Me.UcrValueFlagPeriod5 = New ClimsoftVer4.ucrValueFlagPeriod()
+        Me.UcrValueFlagPeriod6 = New ClimsoftVer4.ucrValueFlagPeriod()
+        Me.UcrValueFlagPeriod7 = New ClimsoftVer4.ucrValueFlagPeriod()
+        Me.UcrValueFlagPeriod8 = New ClimsoftVer4.ucrValueFlagPeriod()
+        Me.UcrValueFlagPeriod9 = New ClimsoftVer4.ucrValueFlagPeriod()
+        Me.UcrValueFlagPeriod10 = New ClimsoftVer4.ucrValueFlagPeriod()
+        Me.UcrValueFlagPeriod11 = New ClimsoftVer4.ucrValueFlagPeriod()
+        Me.UcrValueFlagPeriod12 = New ClimsoftVer4.ucrValueFlagPeriod()
+        Me.lblFlag = New System.Windows.Forms.Label()
+        Me.lblValue = New System.Windows.Forms.Label()
+        Me.lblMonth = New System.Windows.Forms.Label()
+        Me.lbl12 = New System.Windows.Forms.Label()
+        Me.lbl11 = New System.Windows.Forms.Label()
+        Me.lbl10 = New System.Windows.Forms.Label()
+        Me.lbl09 = New System.Windows.Forms.Label()
+        Me.lbl08 = New System.Windows.Forms.Label()
+        Me.lbl07 = New System.Windows.Forms.Label()
+        Me.lbl06 = New System.Windows.Forms.Label()
+        Me.lbl05 = New System.Windows.Forms.Label()
+        Me.lbl04 = New System.Windows.Forms.Label()
+        Me.lbl03 = New System.Windows.Forms.Label()
+        Me.lbl02 = New System.Windows.Forms.Label()
+        Me.lbl01 = New System.Windows.Forms.Label()
+        Me.lblPeriod = New System.Windows.Forms.Label()
+        CType(Me.dtbRecords, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'UcrValueFlagPeriod1
+        '
+        Me.UcrValueFlagPeriod1.Location = New System.Drawing.Point(94, 45)
+        Me.UcrValueFlagPeriod1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.UcrValueFlagPeriod1.Name = "UcrValueFlagPeriod1"
+        Me.UcrValueFlagPeriod1.Size = New System.Drawing.Size(165, 20)
+        Me.UcrValueFlagPeriod1.TabIndex = 0
+        '
+        'UcrValueFlagPeriod2
+        '
+        Me.UcrValueFlagPeriod2.Location = New System.Drawing.Point(94, 162)
+        Me.UcrValueFlagPeriod2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.UcrValueFlagPeriod2.Name = "UcrValueFlagPeriod2"
+        Me.UcrValueFlagPeriod2.Size = New System.Drawing.Size(165, 20)
+        Me.UcrValueFlagPeriod2.TabIndex = 1
+        '
+        'UcrValueFlagPeriod3
+        '
+        Me.UcrValueFlagPeriod3.Location = New System.Drawing.Point(94, 192)
+        Me.UcrValueFlagPeriod3.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.UcrValueFlagPeriod3.Name = "UcrValueFlagPeriod3"
+        Me.UcrValueFlagPeriod3.Size = New System.Drawing.Size(165, 20)
+        Me.UcrValueFlagPeriod3.TabIndex = 2
+        '
+        'UcrValueFlagPeriod4
+        '
+        Me.UcrValueFlagPeriod4.Location = New System.Drawing.Point(94, 370)
+        Me.UcrValueFlagPeriod4.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.UcrValueFlagPeriod4.Name = "UcrValueFlagPeriod4"
+        Me.UcrValueFlagPeriod4.Size = New System.Drawing.Size(165, 20)
+        Me.UcrValueFlagPeriod4.TabIndex = 3
+        '
+        'UcrValueFlagPeriod5
+        '
+        Me.UcrValueFlagPeriod5.Location = New System.Drawing.Point(94, 340)
+        Me.UcrValueFlagPeriod5.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.UcrValueFlagPeriod5.Name = "UcrValueFlagPeriod5"
+        Me.UcrValueFlagPeriod5.Size = New System.Drawing.Size(165, 20)
+        Me.UcrValueFlagPeriod5.TabIndex = 4
+        '
+        'UcrValueFlagPeriod6
+        '
+        Me.UcrValueFlagPeriod6.Location = New System.Drawing.Point(94, 310)
+        Me.UcrValueFlagPeriod6.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.UcrValueFlagPeriod6.Name = "UcrValueFlagPeriod6"
+        Me.UcrValueFlagPeriod6.Size = New System.Drawing.Size(165, 20)
+        Me.UcrValueFlagPeriod6.TabIndex = 5
+        '
+        'UcrValueFlagPeriod7
+        '
+        Me.UcrValueFlagPeriod7.Location = New System.Drawing.Point(94, 222)
+        Me.UcrValueFlagPeriod7.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.UcrValueFlagPeriod7.Name = "UcrValueFlagPeriod7"
+        Me.UcrValueFlagPeriod7.Size = New System.Drawing.Size(165, 20)
+        Me.UcrValueFlagPeriod7.TabIndex = 6
+        '
+        'UcrValueFlagPeriod8
+        '
+        Me.UcrValueFlagPeriod8.Location = New System.Drawing.Point(94, 250)
+        Me.UcrValueFlagPeriod8.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.UcrValueFlagPeriod8.Name = "UcrValueFlagPeriod8"
+        Me.UcrValueFlagPeriod8.Size = New System.Drawing.Size(165, 20)
+        Me.UcrValueFlagPeriod8.TabIndex = 7
+        '
+        'UcrValueFlagPeriod9
+        '
+        Me.UcrValueFlagPeriod9.Location = New System.Drawing.Point(94, 280)
+        Me.UcrValueFlagPeriod9.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.UcrValueFlagPeriod9.Name = "UcrValueFlagPeriod9"
+        Me.UcrValueFlagPeriod9.Size = New System.Drawing.Size(165, 20)
+        Me.UcrValueFlagPeriod9.TabIndex = 8
+        '
+        'UcrValueFlagPeriod10
+        '
+        Me.UcrValueFlagPeriod10.Location = New System.Drawing.Point(94, 132)
+        Me.UcrValueFlagPeriod10.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.UcrValueFlagPeriod10.Name = "UcrValueFlagPeriod10"
+        Me.UcrValueFlagPeriod10.Size = New System.Drawing.Size(165, 20)
+        Me.UcrValueFlagPeriod10.TabIndex = 9
+        '
+        'UcrValueFlagPeriod11
+        '
+        Me.UcrValueFlagPeriod11.Location = New System.Drawing.Point(94, 105)
+        Me.UcrValueFlagPeriod11.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.UcrValueFlagPeriod11.Name = "UcrValueFlagPeriod11"
+        Me.UcrValueFlagPeriod11.Size = New System.Drawing.Size(165, 20)
+        Me.UcrValueFlagPeriod11.TabIndex = 10
+        '
+        'UcrValueFlagPeriod12
+        '
+        Me.UcrValueFlagPeriod12.Location = New System.Drawing.Point(94, 75)
+        Me.UcrValueFlagPeriod12.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.UcrValueFlagPeriod12.Name = "UcrValueFlagPeriod12"
+        Me.UcrValueFlagPeriod12.Size = New System.Drawing.Size(165, 20)
+        Me.UcrValueFlagPeriod12.TabIndex = 11
+        '
+        'lblFlag
+        '
+        Me.lblFlag.AutoSize = True
+        Me.lblFlag.Location = New System.Drawing.Point(154, 28)
+        Me.lblFlag.Name = "lblFlag"
+        Me.lblFlag.Size = New System.Drawing.Size(30, 13)
+        Me.lblFlag.TabIndex = 572
+        Me.lblFlag.Text = "Flag:"
+        '
+        'lblValue
+        '
+        Me.lblValue.AutoSize = True
+        Me.lblValue.Location = New System.Drawing.Point(96, 28)
+        Me.lblValue.Name = "lblValue"
+        Me.lblValue.Size = New System.Drawing.Size(37, 13)
+        Me.lblValue.TabIndex = 571
+        Me.lblValue.Text = "Value:"
+        '
+        'lblMonth
+        '
+        Me.lblMonth.AutoSize = True
+        Me.lblMonth.Location = New System.Drawing.Point(51, 27)
+        Me.lblMonth.Name = "lblMonth"
+        Me.lblMonth.Size = New System.Drawing.Size(40, 13)
+        Me.lblMonth.TabIndex = 570
+        Me.lblMonth.Text = "Month:"
+        '
+        'lbl12
+        '
+        Me.lbl12.AutoSize = True
+        Me.lbl12.Location = New System.Drawing.Point(72, 374)
+        Me.lbl12.Name = "lbl12"
+        Me.lbl12.Size = New System.Drawing.Size(19, 13)
+        Me.lbl12.TabIndex = 584
+        Me.lbl12.Text = "12"
+        '
+        'lbl11
+        '
+        Me.lbl11.AutoSize = True
+        Me.lbl11.Location = New System.Drawing.Point(72, 344)
+        Me.lbl11.Name = "lbl11"
+        Me.lbl11.Size = New System.Drawing.Size(19, 13)
+        Me.lbl11.TabIndex = 583
+        Me.lbl11.Text = "11"
+        '
+        'lbl10
+        '
+        Me.lbl10.AutoSize = True
+        Me.lbl10.Location = New System.Drawing.Point(72, 313)
+        Me.lbl10.Name = "lbl10"
+        Me.lbl10.Size = New System.Drawing.Size(19, 13)
+        Me.lbl10.TabIndex = 582
+        Me.lbl10.Text = "10"
+        '
+        'lbl09
+        '
+        Me.lbl09.AutoSize = True
+        Me.lbl09.Location = New System.Drawing.Point(72, 283)
+        Me.lbl09.Name = "lbl09"
+        Me.lbl09.Size = New System.Drawing.Size(19, 13)
+        Me.lbl09.TabIndex = 581
+        Me.lbl09.Text = "09"
+        '
+        'lbl08
+        '
+        Me.lbl08.AutoSize = True
+        Me.lbl08.Location = New System.Drawing.Point(72, 254)
+        Me.lbl08.Name = "lbl08"
+        Me.lbl08.Size = New System.Drawing.Size(19, 13)
+        Me.lbl08.TabIndex = 580
+        Me.lbl08.Text = "08"
+        '
+        'lbl07
+        '
+        Me.lbl07.AutoSize = True
+        Me.lbl07.Location = New System.Drawing.Point(72, 226)
+        Me.lbl07.Name = "lbl07"
+        Me.lbl07.Size = New System.Drawing.Size(19, 13)
+        Me.lbl07.TabIndex = 579
+        Me.lbl07.Text = "07"
+        '
+        'lbl06
+        '
+        Me.lbl06.AutoSize = True
+        Me.lbl06.Location = New System.Drawing.Point(72, 196)
+        Me.lbl06.Name = "lbl06"
+        Me.lbl06.Size = New System.Drawing.Size(19, 13)
+        Me.lbl06.TabIndex = 578
+        Me.lbl06.Text = "06"
+        '
+        'lbl05
+        '
+        Me.lbl05.AutoSize = True
+        Me.lbl05.Location = New System.Drawing.Point(72, 166)
+        Me.lbl05.Name = "lbl05"
+        Me.lbl05.Size = New System.Drawing.Size(19, 13)
+        Me.lbl05.TabIndex = 577
+        Me.lbl05.Text = "05"
+        '
+        'lbl04
+        '
+        Me.lbl04.AutoSize = True
+        Me.lbl04.Location = New System.Drawing.Point(72, 136)
+        Me.lbl04.Name = "lbl04"
+        Me.lbl04.Size = New System.Drawing.Size(19, 13)
+        Me.lbl04.TabIndex = 576
+        Me.lbl04.Text = "04"
+        '
+        'lbl03
+        '
+        Me.lbl03.AutoSize = True
+        Me.lbl03.Location = New System.Drawing.Point(72, 109)
+        Me.lbl03.Name = "lbl03"
+        Me.lbl03.Size = New System.Drawing.Size(19, 13)
+        Me.lbl03.TabIndex = 575
+        Me.lbl03.Text = "03"
+        '
+        'lbl02
+        '
+        Me.lbl02.AutoSize = True
+        Me.lbl02.Location = New System.Drawing.Point(72, 79)
+        Me.lbl02.Name = "lbl02"
+        Me.lbl02.Size = New System.Drawing.Size(19, 13)
+        Me.lbl02.TabIndex = 574
+        Me.lbl02.Text = "02"
+        '
+        'lbl01
+        '
+        Me.lbl01.AutoSize = True
+        Me.lbl01.Location = New System.Drawing.Point(71, 49)
+        Me.lbl01.Name = "lbl01"
+        Me.lbl01.Size = New System.Drawing.Size(19, 13)
+        Me.lbl01.TabIndex = 573
+        Me.lbl01.Text = "01"
+        '
+        'lblPeriod
+        '
+        Me.lblPeriod.AutoSize = True
+        Me.lblPeriod.Location = New System.Drawing.Point(210, 29)
+        Me.lblPeriod.Name = "lblPeriod"
+        Me.lblPeriod.Size = New System.Drawing.Size(40, 13)
+        Me.lblPeriod.TabIndex = 585
+        Me.lblPeriod.Text = "Period:"
+        '
+        'ucrMonthlydata
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.Controls.Add(Me.lblPeriod)
+        Me.Controls.Add(Me.lbl12)
+        Me.Controls.Add(Me.lbl11)
+        Me.Controls.Add(Me.lbl10)
+        Me.Controls.Add(Me.lbl09)
+        Me.Controls.Add(Me.lbl08)
+        Me.Controls.Add(Me.lbl07)
+        Me.Controls.Add(Me.lbl06)
+        Me.Controls.Add(Me.lbl05)
+        Me.Controls.Add(Me.lbl04)
+        Me.Controls.Add(Me.lbl03)
+        Me.Controls.Add(Me.lbl02)
+        Me.Controls.Add(Me.lbl01)
+        Me.Controls.Add(Me.lblFlag)
+        Me.Controls.Add(Me.lblValue)
+        Me.Controls.Add(Me.lblMonth)
+        Me.Controls.Add(Me.UcrValueFlagPeriod12)
+        Me.Controls.Add(Me.UcrValueFlagPeriod11)
+        Me.Controls.Add(Me.UcrValueFlagPeriod10)
+        Me.Controls.Add(Me.UcrValueFlagPeriod9)
+        Me.Controls.Add(Me.UcrValueFlagPeriod8)
+        Me.Controls.Add(Me.UcrValueFlagPeriod7)
+        Me.Controls.Add(Me.UcrValueFlagPeriod6)
+        Me.Controls.Add(Me.UcrValueFlagPeriod5)
+        Me.Controls.Add(Me.UcrValueFlagPeriod4)
+        Me.Controls.Add(Me.UcrValueFlagPeriod3)
+        Me.Controls.Add(Me.UcrValueFlagPeriod2)
+        Me.Controls.Add(Me.UcrValueFlagPeriod1)
+        Me.Name = "ucrMonthlydata"
+        Me.Size = New System.Drawing.Size(320, 401)
+        CType(Me.dtbRecords, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents UcrValueFlagPeriod1 As ucrValueFlagPeriod
+    Friend WithEvents UcrValueFlagPeriod2 As ucrValueFlagPeriod
+    Friend WithEvents UcrValueFlagPeriod3 As ucrValueFlagPeriod
+    Friend WithEvents UcrValueFlagPeriod4 As ucrValueFlagPeriod
+    Friend WithEvents UcrValueFlagPeriod5 As ucrValueFlagPeriod
+    Friend WithEvents UcrValueFlagPeriod6 As ucrValueFlagPeriod
+    Friend WithEvents UcrValueFlagPeriod7 As ucrValueFlagPeriod
+    Friend WithEvents UcrValueFlagPeriod8 As ucrValueFlagPeriod
+    Friend WithEvents UcrValueFlagPeriod9 As ucrValueFlagPeriod
+    Friend WithEvents UcrValueFlagPeriod10 As ucrValueFlagPeriod
+    Friend WithEvents UcrValueFlagPeriod11 As ucrValueFlagPeriod
+    Friend WithEvents UcrValueFlagPeriod12 As ucrValueFlagPeriod
+    Friend WithEvents lblFlag As Label
+    Friend WithEvents lblValue As Label
+    Friend WithEvents lblMonth As Label
+    Friend WithEvents lbl12 As Label
+    Friend WithEvents lbl11 As Label
+    Friend WithEvents lbl10 As Label
+    Friend WithEvents lbl09 As Label
+    Friend WithEvents lbl08 As Label
+    Friend WithEvents lbl07 As Label
+    Friend WithEvents lbl06 As Label
+    Friend WithEvents lbl05 As Label
+    Friend WithEvents lbl04 As Label
+    Friend WithEvents lbl03 As Label
+    Friend WithEvents lbl02 As Label
+    Friend WithEvents lbl01 As Label
+    Friend WithEvents lblPeriod As Label
+End Class
