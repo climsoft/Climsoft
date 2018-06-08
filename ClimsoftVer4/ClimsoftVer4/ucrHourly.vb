cea81cb630dfe2ae75c182a25c2ae40809cd4af3
patowhiz <conlooptechnologies@gmail.com>
2018-06-08 10:47:31 +0300

    check total enhancement

--- ClimsoftVer4/ClimsoftVer4/ucrHourly.vb
+++ ClimsoftVer4/ClimsoftVer4/ucrHourly.vb
@@ -246,6 +246,12 @@ Public Class ucrHourly
                     ucrInputTotal.SetBackColor(Color.Cyan)
                     bValueCorrect = False
                 End If
+                bValueCorrect = (elemTotal = expectedTotal)
+                If Not bValueCorrect Then
+                    MessageBox.Show("Value in [Total] textbox is different from that calculated by computer! The computed total is " & elemTotal, "Error in total", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    ucrInputTotal.SetBackColor(Color.Cyan)
+                End If
+
             End If
         Else
             bValueCorrect = True
