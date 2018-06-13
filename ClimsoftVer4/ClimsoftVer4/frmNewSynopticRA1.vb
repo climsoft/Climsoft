42095993164ceb7e49331347933da585157aeb4c
patowhiz <conlooptechnologies@gmail.com>
2018-06-13 09:04:33 +0300

    sub GoToNextControl addition

--- ClimsoftVer4/ClimsoftVer4/frmNewSynopticRA1.vb
+++ ClimsoftVer4/ClimsoftVer4/frmNewSynopticRA1.vb
@@ -16,6 +16,7 @@
         ucrNavigation.PopulateControl()
 
         SaveEnable()
+        SetControlsKeyDownListners()
     End Sub
 
     Private Sub btnAddNew_Click(sender As Object, e As EventArgs) Handles btnAddNew.Click
@@ -98,31 +99,8 @@
         SaveEnable()
     End Sub
 
-    'This is from Samuel's code
-    Private Sub btnHelp_Click(sender As Object, e As EventArgs) Handles btnHelp.Click
-        Help.ShowHelp(Me, Application.StartupPath & "\climsoft4.chm", "keyentryoperations.htm#form_synopticRA1")
-    End Sub
-
-    'This is from Samuel's code
-    Private Sub btnView_Click(sender As Object, e As EventArgs) Handles btnView.Click
-        Dim viewRecords As New dataEntryGlobalRoutines
-        Dim sql, userName As String
-        userName = frmLogin.txtUsername.Text
-        dsSourceTableName = "form_synoptic_2_RA1"
-        If userGroup = "ClimsoftOperator" Or userGroup = "ClimsoftRainfall" Then
-            sql = "SELECT * FROM form_synoptic_2_RA1 where signature ='" & userName & "' ORDER by stationId,yyyy,mm,dd,hh;"
-        Else
-            sql = "SELECT * FROM form_synoptic_2_RA1 ORDER by stationId,yyyy,mm,dd,hh;"
-        End If
-        viewRecords.viewTableRecords(sql)
-    End Sub
-
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
         Me.Close()
-    End Sub
-
-    Private Sub btnUpload_Click(sender As Object, e As EventArgs) Handles btnUpload.Click
-        'TODO
     End Sub
 
     Private Sub SaveEnable()
@@ -177,5 +155,133 @@
 
         Return True
     End Function
+
+    ''' <summary>
+    ''' sets key down listeners for the form controls
+    ''' </summary>
+    Private Sub SetControlsKeyDownListners()
+        For Each formCtr As Control In Me.Controls
+            If TypeOf formCtr Is ucrBaseDataLink Then
+                If TypeOf formCtr Is ucrSynopticRA1 Then
+                    Dim synopticControl As ucrSynopticRA1 = DirectCast(formCtr, ucrSynopticRA1)
+                    For Each synopCtl As Control In synopticControl.Controls
+                        If TypeOf synopCtl Is ucrValueFlagPeriod Then
+                            AddHandler DirectCast(synopCtl, ucrValueFlagPeriod).evtKeyDown, AddressOf GoToNextControl
+                        End If
+                    Next
+                Else
+                    AddHandler DirectCast(formCtr, ucrBaseDataLink).evtKeyDown, AddressOf GoToNextControl
+                End If
+            End If
+
+        Next
+    End Sub
+
+    Private Sub GoToNextControl(sender As Object, e As KeyEventArgs)
+        If e.KeyCode = Keys.Enter Then
+
+            If TypeOf sender Is ucrSynopticRA1 Then
+                Dim oo = True
+            End If
+
+            If TypeOf sender Is ucrValueFlagPeriod Then
+                Dim l = True
+            End If
+
+            If TypeOf sender Is ucrValueFlagPeriod Then
+                If DirectCast(sender, ucrValueFlagPeriod).PreValidateValue() Then
+                    Me.SelectNextControl(sender, True, True, True, True)
+                End If
+            ElseIf TypeOf sender Is ucrBaseDataLink Then
+                If DirectCast(sender, ucrBaseDataLink).ValidateValue() Then
+                    Me.SelectNextControl(sender, True, True, True, True)
+                End If
+            End If
+
+
+
+
+
+
+
+        End If
+
+
+
+    End Sub
+
+    'This is from Samuel's code
+    Private Sub btnHelp_Click(sender As Object, e As EventArgs) Handles btnHelp.Click
+        Help.ShowHelp(Me, Application.StartupPath & "\climsoft4.chm", "keyentryoperations.htm#form_synopticRA1")
+    End Sub
+
+    'TODO. This is from Samuel's code
+    Private Sub btnView_Click(sender As Object, e As EventArgs) Handles btnView.Click
+        Dim viewRecords As New dataEntryGlobalRoutines
+        Dim sql, userName As String
+        userName = frmLogin.txtUsername.Text
+        dsSourceTableName = "form_synoptic_2_RA1"
+        If userGroup = "ClimsoftOperator" Or userGroup = "ClimsoftRainfall" Then
+            sql = "SELECT * FROM form_synoptic_2_RA1 where signature ='" & userName & "' ORDER by stationId,yyyy,mm,dd,hh;"
+        Else
+            sql = "SELECT * FROM form_synoptic_2_RA1 ORDER by stationId,yyyy,mm,dd,hh;"
+        End If
+        viewRecords.viewTableRecords(sql)
+    End Sub
+
+    Private Sub btnUpload_Click(sender As Object, e As EventArgs) Handles btnUpload.Click
+        'TODO
+    End Sub
+
+    'TODO. Copied from Samuel's code
+    Private Sub btnTDCF_Click(sender As Object, e As EventArgs) Handles btnTDCF.Click
+        frmSynopTDCF.Show()
+        frmSynopTDCF.cboTemplate.Text = "TM_307081"
+        ' Subset Observations
+        SubsetObservations()
+    End Sub
+
+    'TODO. Copied from Samuel's code
+    Sub SubsetObservations()
+        Dim kount As Integer
+        Dim myConnectionString As String
+        Dim conn As New MySql.Data.MySqlClient.MySqlConnection
+        Dim sql As String
+        Dim da As MySql.Data.MySqlClient.MySqlDataAdapter
+        Dim ds As New DataSet
+        Try
+            myConnectionString = frmLogin.txtusrpwd.Text
+            conn.ConnectionString = myConnectionString
+            conn.Open()
+
+            ' Get all stations with the same observation time to constitute the subset of stations for encoding
+            sql = "SELECT stationId, yyyy, mm, dd, hh from form_synoptic_2_ra1 where yyyy = '" & ucrYearSelector.GetValue & "' and mm = '" & ucrMonth.GetValue & "' and dd = '" & ucrDay.GetValue & "' and hh = '" & ucrHour.GetValue & "';"
+
+            'MsgBox(sql)
+            da = New MySql.Data.MySqlClient.MySqlDataAdapter(sql, conn)
+            ds.Clear()
+            da.Fill(ds, "form_synoptic_2_ra1")
+            kount = ds.Tables("form_synoptic_2_ra1").Rows.Count
+
+            frmSynopTDCF.cboStation.Text = ucrStationSelector.GetValue
+            frmSynopTDCF.txtYear.Text = ucrYearSelector.GetValue
+            frmSynopTDCF.cboMonth.Text = ucrMonth.GetValue
+            frmSynopTDCF.cboDay.Text = ucrDay.GetValue
+            frmSynopTDCF.cboHour.Text = ucrHour.GetValue
+
+            ' Populate the station combo box with the stations for the subset
+            For i = 0 To kount - 1
+                'MsgBox(ds.Tables("form_synoptic_2_ra1").Rows(i).Item("stationId"))
+                frmSynopTDCF.cboStation.Items.Add(ds.Tables("form_synoptic_2_ra1").Rows(i).Item("stationId"))
+            Next
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            conn.Close()
+        End Try
+        conn.Close()
+    End Sub
+
+
 
 End Class
