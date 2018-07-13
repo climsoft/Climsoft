b2da3dd977485ec4c261c6d41d5e1b99da86caf9
maxwell fundi <maxwellfundi@live.com>
2018-07-13 16:18:36 +0300

    form monthly

--- ClimsoftVer4/ClimsoftVer4/frmNewMonthly.Designer.vb
+++ ClimsoftVer4/ClimsoftVer4/frmNewMonthly.Designer.vb
@@ -47,39 +47,35 @@ Partial Class frmNewMonthly
         'lblYear
         '
         lblYear.AutoSize = True
-        lblYear.Location = New System.Drawing.Point(701, 17)
-        lblYear.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        lblYear.Location = New System.Drawing.Point(467, 11)
         lblYear.Name = "lblYear"
-        lblYear.Size = New System.Drawing.Size(47, 20)
+        lblYear.Size = New System.Drawing.Size(32, 13)
         lblYear.TabIndex = 494
         lblYear.Text = "Year:"
         '
         'lblElement
         '
         Me.lblElement.AutoSize = True
-        Me.lblElement.Location = New System.Drawing.Point(396, 17)
-        Me.lblElement.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblElement.Location = New System.Drawing.Point(264, 11)
         Me.lblElement.Name = "lblElement"
-        Me.lblElement.Size = New System.Drawing.Size(72, 20)
+        Me.lblElement.Size = New System.Drawing.Size(48, 13)
         Me.lblElement.TabIndex = 496
         Me.lblElement.Text = "Element:"
         '
         'lblStation
         '
         Me.lblStation.AutoSize = True
-        Me.lblStation.Location = New System.Drawing.Point(102, 17)
-        Me.lblStation.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblStation.Location = New System.Drawing.Point(68, 11)
         Me.lblStation.Name = "lblStation"
-        Me.lblStation.Size = New System.Drawing.Size(64, 20)
+        Me.lblStation.Size = New System.Drawing.Size(43, 13)
         Me.lblStation.TabIndex = 495
         Me.lblStation.Text = "Station:"
         '
         'btnView
         '
-        Me.btnView.Location = New System.Drawing.Point(632, 765)
-        Me.btnView.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnView.Location = New System.Drawing.Point(421, 481)
         Me.btnView.Name = "btnView"
-        Me.btnView.Size = New System.Drawing.Size(112, 35)
+        Me.btnView.Size = New System.Drawing.Size(75, 23)
         Me.btnView.TabIndex = 11
         Me.btnView.Text = "View"
         Me.btnView.UseVisualStyleBackColor = True
@@ -87,10 +83,9 @@ Partial Class frmNewMonthly
         'btnUpload
         '
         Me.btnUpload.BackColor = System.Drawing.Color.Lime
-        Me.btnUpload.Location = New System.Drawing.Point(874, 808)
-        Me.btnUpload.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnUpload.Location = New System.Drawing.Point(583, 509)
         Me.btnUpload.Name = "btnUpload"
-        Me.btnUpload.Size = New System.Drawing.Size(112, 35)
+        Me.btnUpload.Size = New System.Drawing.Size(75, 23)
         Me.btnUpload.TabIndex = 14
         Me.btnUpload.Text = "Upload"
         Me.btnUpload.UseVisualStyleBackColor = False
@@ -98,137 +93,128 @@ Partial Class frmNewMonthly
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(270, 823)
-        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Location = New System.Drawing.Point(180, 519)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(87, 20)
+        Me.Label5.Size = New System.Drawing.Size(59, 13)
         Me.Label5.TabIndex = 675
         Me.Label5.Text = "Sequencer"
         '
         'txtSequencer
         '
-        Me.txtSequencer.Location = New System.Drawing.Point(368, 818)
-        Me.txtSequencer.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtSequencer.Location = New System.Drawing.Point(245, 516)
         Me.txtSequencer.Name = "txtSequencer"
-        Me.txtSequencer.Size = New System.Drawing.Size(298, 26)
+        Me.txtSequencer.Size = New System.Drawing.Size(200, 20)
         Me.txtSequencer.TabIndex = 674
         Me.txtSequencer.Text = "seq_year"
         '
         'btnHelp
         '
-        Me.btnHelp.Location = New System.Drawing.Point(874, 765)
-        Me.btnHelp.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnHelp.Location = New System.Drawing.Point(583, 481)
         Me.btnHelp.Name = "btnHelp"
-        Me.btnHelp.Size = New System.Drawing.Size(112, 35)
+        Me.btnHelp.Size = New System.Drawing.Size(75, 23)
         Me.btnHelp.TabIndex = 13
         Me.btnHelp.Text = "Help"
         Me.btnHelp.UseVisualStyleBackColor = True
         '
         'btnClear
         '
-        Me.btnClear.Location = New System.Drawing.Point(510, 765)
-        Me.btnClear.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnClear.Location = New System.Drawing.Point(340, 481)
         Me.btnClear.Name = "btnClear"
-        Me.btnClear.Size = New System.Drawing.Size(112, 35)
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
         Me.btnClear.TabIndex = 10
         Me.btnClear.Text = "Clear"
         Me.btnClear.UseVisualStyleBackColor = True
         '
         'btnCommit
         '
-        Me.btnCommit.Location = New System.Drawing.Point(146, 765)
-        Me.btnCommit.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnCommit.Location = New System.Drawing.Point(97, 481)
         Me.btnCommit.Name = "btnCommit"
-        Me.btnCommit.Size = New System.Drawing.Size(112, 35)
+        Me.btnCommit.Size = New System.Drawing.Size(75, 23)
         Me.btnCommit.TabIndex = 8
         Me.btnCommit.Text = "Save"
         Me.btnCommit.UseVisualStyleBackColor = True
         '
         'btnDelete
         '
-        Me.btnDelete.Location = New System.Drawing.Point(388, 765)
-        Me.btnDelete.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnDelete.Location = New System.Drawing.Point(259, 481)
         Me.btnDelete.Name = "btnDelete"
-        Me.btnDelete.Size = New System.Drawing.Size(112, 35)
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
         Me.btnDelete.TabIndex = 9
         Me.btnDelete.Text = "Delete"
         Me.btnDelete.UseVisualStyleBackColor = True
         '
         'btnAddNew
         '
-        Me.btnAddNew.Location = New System.Drawing.Point(24, 765)
-        Me.btnAddNew.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnAddNew.Location = New System.Drawing.Point(16, 481)
         Me.btnAddNew.Name = "btnAddNew"
-        Me.btnAddNew.Size = New System.Drawing.Size(112, 35)
+        Me.btnAddNew.Size = New System.Drawing.Size(75, 23)
         Me.btnAddNew.TabIndex = 7
         Me.btnAddNew.Text = "Add New"
         Me.btnAddNew.UseVisualStyleBackColor = True
         '
         'btnUpdate
         '
-        Me.btnUpdate.Location = New System.Drawing.Point(267, 765)
-        Me.btnUpdate.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnUpdate.Location = New System.Drawing.Point(178, 481)
         Me.btnUpdate.Name = "btnUpdate"
-        Me.btnUpdate.Size = New System.Drawing.Size(112, 35)
+        Me.btnUpdate.Size = New System.Drawing.Size(75, 23)
         Me.btnUpdate.TabIndex = 6
         Me.btnUpdate.Text = "Update"
         Me.btnUpdate.UseVisualStyleBackColor = True
         '
         'btnClose
         '
-        Me.btnClose.Location = New System.Drawing.Point(753, 765)
-        Me.btnClose.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnClose.Location = New System.Drawing.Point(502, 481)
         Me.btnClose.Name = "btnClose"
-        Me.btnClose.Size = New System.Drawing.Size(112, 35)
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
         Me.btnClose.TabIndex = 12
         Me.btnClose.Text = "Close"
         Me.btnClose.UseVisualStyleBackColor = True
         '
         'ucrNavigation
         '
-        Me.ucrNavigation.Location = New System.Drawing.Point(263, 714)
-        Me.ucrNavigation.Margin = New System.Windows.Forms.Padding(6, 8, 6, 8)
+        Me.ucrNavigation.Location = New System.Drawing.Point(163, 442)
+        Me.ucrNavigation.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.ucrNavigation.Name = "ucrNavigation"
-        Me.ucrNavigation.Size = New System.Drawing.Size(504, 39)
+        Me.ucrNavigation.Size = New System.Drawing.Size(336, 25)
         Me.ucrNavigation.TabIndex = 15
         '
         'ucrMonthlydata
         '
-        Me.ucrMonthlydata.Location = New System.Drawing.Point(270, 81)
-        Me.ucrMonthlydata.Margin = New System.Windows.Forms.Padding(9, 12, 9, 12)
+        Me.ucrMonthlydata.Location = New System.Drawing.Point(180, 53)
+        Me.ucrMonthlydata.Margin = New System.Windows.Forms.Padding(6, 8, 6, 8)
         Me.ucrMonthlydata.Name = "ucrMonthlydata"
-        Me.ucrMonthlydata.Size = New System.Drawing.Size(514, 622)
+        Me.ucrMonthlydata.Size = New System.Drawing.Size(343, 404)
         Me.ucrMonthlydata.TabIndex = 4
         '
         'ucrYearSelector
         '
-        Me.ucrYearSelector.Location = New System.Drawing.Point(705, 41)
-        Me.ucrYearSelector.Margin = New System.Windows.Forms.Padding(14, 18, 14, 18)
+        Me.ucrYearSelector.Location = New System.Drawing.Point(470, 27)
+        Me.ucrYearSelector.Margin = New System.Windows.Forms.Padding(9, 12, 9, 12)
         Me.ucrYearSelector.Name = "ucrYearSelector"
-        Me.ucrYearSelector.Size = New System.Drawing.Size(128, 38)
+        Me.ucrYearSelector.Size = New System.Drawing.Size(85, 25)
         Me.ucrYearSelector.TabIndex = 3
         '
         'ucrElementSelector
         '
-        Me.ucrElementSelector.Location = New System.Drawing.Point(400, 42)
-        Me.ucrElementSelector.Margin = New System.Windows.Forms.Padding(9, 12, 9, 12)
+        Me.ucrElementSelector.Location = New System.Drawing.Point(267, 27)
+        Me.ucrElementSelector.Margin = New System.Windows.Forms.Padding(6, 8, 6, 8)
         Me.ucrElementSelector.Name = "ucrElementSelector"
-        Me.ucrElementSelector.Size = New System.Drawing.Size(267, 32)
+        Me.ucrElementSelector.Size = New System.Drawing.Size(178, 21)
         Me.ucrElementSelector.TabIndex = 2
         '
         'ucrStationSelector
         '
-        Me.ucrStationSelector.Location = New System.Drawing.Point(97, 42)
-        Me.ucrStationSelector.Margin = New System.Windows.Forms.Padding(14, 18, 14, 18)
+        Me.ucrStationSelector.Location = New System.Drawing.Point(65, 27)
+        Me.ucrStationSelector.Margin = New System.Windows.Forms.Padding(9, 12, 9, 12)
         Me.ucrStationSelector.Name = "ucrStationSelector"
-        Me.ucrStationSelector.Size = New System.Drawing.Size(280, 37)
+        Me.ucrStationSelector.Size = New System.Drawing.Size(187, 24)
         Me.ucrStationSelector.TabIndex = 1
         '
         'frmNewMonthly
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1022, 866)
+        Me.ClientSize = New System.Drawing.Size(681, 543)
         Me.Controls.Add(Me.ucrNavigation)
         Me.Controls.Add(Me.ucrMonthlydata)
         Me.Controls.Add(Me.btnView)
@@ -248,7 +234,7 @@ Partial Class frmNewMonthly
         Me.Controls.Add(Me.lblElement)
         Me.Controls.Add(Me.lblStation)
         Me.Controls.Add(lblYear)
-        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "frmNewMonthly"
