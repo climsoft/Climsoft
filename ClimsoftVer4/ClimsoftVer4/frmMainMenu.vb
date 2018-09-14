1125af3171c0b59415725c4e179e1c64a6057f27
smachua <smmachua@yahoo.com>
2018-09-14 14:41:51 +0300

    updates-14092018
    Updates on
    -main menu close button
    - version 4.1. lablel
    - data forms

--- ClimsoftVer4/ClimsoftVer4/frmMainMenu.vb
+++ ClimsoftVer4/ClimsoftVer4/frmMainMenu.vb
@@ -99,7 +99,7 @@ Public Class frmMainMenu
                     cmdKeyEntry.Enabled = False
                     cmdPaperArchive.Enabled = False
                     cmdProducts.Enabled = False
-
+                    cmdMetadata.Enabled = False
                 End If
             End If
         Catch ex As Exception
@@ -314,5 +314,9 @@ Public Class frmMainMenu
             .Show()
         End With
 
+    End Sub
+
+    Private Sub frmMainMenu_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        End
     End Sub
 End Class
