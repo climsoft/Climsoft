9028b610bac23ff3d27afb7c7d2a8756f8a2a753
Lazarus Kioko Muthenya <muthenyakioko@gmail.com>
2018-11-21 16:22:21 +0300

    Minor edit

--- ClimsoftVer4/ClimsoftVer4/ucrMetadataInstrument.Designer.vb
+++ ClimsoftVer4/ClimsoftVer4/ucrMetadataInstrument.Designer.vb
@@ -22,8 +22,18 @@ Partial Class ucrMetadataInstrument
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        CType(Me.dtbRecords, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ucrMetadataInstrument
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Name = "ucrMetadataInstrument"
+        Me.Size = New System.Drawing.Size(588, 406)
+        CType(Me.dtbRecords, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
