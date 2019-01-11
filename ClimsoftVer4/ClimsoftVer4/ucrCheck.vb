02bb33a6eb765e3c409673d46ab392833d872173
Lazarus Kioko Muthenya <muthenyakioko@gmail.com>
2019-01-11 18:07:57 +0300

    Validation and checkbox additions

--- ClimsoftVer4/ClimsoftVer4/ucrCheck.vb
+++ ClimsoftVer4/ClimsoftVer4/ucrCheck.vb
@@ -1,3 +1,10 @@
 ﻿Public Class ucrCheck
-
+    Public Property CheckBoxText() As String
+        Get
+            Return chkCheck.Text
+        End Get
+        Set(value As String)
+            chkCheck.Text = value
+        End Set
+    End Property
 End Class
