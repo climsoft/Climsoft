1a41f576582d7b200724aecf850f1c0958646142
patowhiz <conlooptechnologies@gmail.com>
2019-09-02 18:39:12 +0300

    Settings - Data Forms Activation

--- ClimsoftVer4/ClimsoftVer4/frmDataForms.Designer.vb
+++ ClimsoftVer4/ClimsoftVer4/frmDataForms.Designer.vb
@@ -22,71 +22,97 @@ Partial Class frmDataForms
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.lstvForms = New System.Windows.Forms.ListView()
-        Me.cmdApply = New System.Windows.Forms.Button()
-        Me.cmdHelp = New System.Windows.Forms.Button()
-        Me.cmdCancel = New System.Windows.Forms.Button()
+        Me.lstViewForms = New System.Windows.Forms.ListView()
+        Me.formName = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.formDescription = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.btnApply = New System.Windows.Forms.Button()
+        Me.btnHelp = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.lblSelect = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'lstvForms
+        'lstViewForms
         '
-        Me.lstvForms.AllowColumnReorder = True
-        Me.lstvForms.AllowDrop = True
-        Me.lstvForms.CheckBoxes = True
-        Me.lstvForms.FullRowSelect = True
-        Me.lstvForms.GridLines = True
-        Me.lstvForms.LabelEdit = True
-        Me.lstvForms.Location = New System.Drawing.Point(12, 12)
-        Me.lstvForms.Name = "lstvForms"
-        Me.lstvForms.RightToLeft = System.Windows.Forms.RightToLeft.Yes
-        Me.lstvForms.Size = New System.Drawing.Size(685, 329)
-        Me.lstvForms.TabIndex = 0
-        Me.lstvForms.UseCompatibleStateImageBehavior = False
-        Me.lstvForms.View = System.Windows.Forms.View.Details
+        Me.lstViewForms.AllowDrop = True
+        Me.lstViewForms.CheckBoxes = True
+        Me.lstViewForms.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.formName, Me.formDescription})
+        Me.lstViewForms.FullRowSelect = True
+        Me.lstViewForms.HideSelection = False
+        Me.lstViewForms.Location = New System.Drawing.Point(4, 27)
+        Me.lstViewForms.Name = "lstViewForms"
+        Me.lstViewForms.Size = New System.Drawing.Size(554, 238)
+        Me.lstViewForms.TabIndex = 0
+        Me.lstViewForms.UseCompatibleStateImageBehavior = False
+        Me.lstViewForms.View = System.Windows.Forms.View.Details
         '
-        'cmdApply
+        'formName
         '
-        Me.cmdApply.Location = New System.Drawing.Point(422, 347)
-        Me.cmdApply.Name = "cmdApply"
-        Me.cmdApply.Size = New System.Drawing.Size(69, 27)
-        Me.cmdApply.TabIndex = 1
-        Me.cmdApply.Text = "Apply"
-        Me.cmdApply.UseVisualStyleBackColor = True
+        Me.formName.Text = "Form Name"
+        Me.formName.Width = 150
         '
-        'cmdHelp
+        'formDescription
         '
-        Me.cmdHelp.Location = New System.Drawing.Point(614, 347)
-        Me.cmdHelp.Name = "cmdHelp"
-        Me.cmdHelp.Size = New System.Drawing.Size(69, 27)
-        Me.cmdHelp.TabIndex = 2
-        Me.cmdHelp.Text = "Help"
-        Me.cmdHelp.UseVisualStyleBackColor = True
+        Me.formDescription.Text = "Description"
+        Me.formDescription.Width = 400
         '
-        'cmdCancel
+        'btnApply
         '
-        Me.cmdCancel.Location = New System.Drawing.Point(518, 347)
-        Me.cmdCancel.Name = "cmdCancel"
-        Me.cmdCancel.Size = New System.Drawing.Size(69, 27)
-        Me.cmdCancel.TabIndex = 3
-        Me.cmdCancel.Text = "Cancel"
-        Me.cmdCancel.UseVisualStyleBackColor = True
+        Me.btnApply.Location = New System.Drawing.Point(338, 271)
+        Me.btnApply.Name = "btnApply"
+        Me.btnApply.Size = New System.Drawing.Size(69, 27)
+        Me.btnApply.TabIndex = 1
+        Me.btnApply.Text = "Apply"
+        Me.btnApply.UseVisualStyleBackColor = True
+        '
+        'btnHelp
+        '
+        Me.btnHelp.Location = New System.Drawing.Point(488, 271)
+        Me.btnHelp.Name = "btnHelp"
+        Me.btnHelp.Size = New System.Drawing.Size(69, 27)
+        Me.btnHelp.TabIndex = 2
+        Me.btnHelp.Text = "Help"
+        Me.btnHelp.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(413, 271)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(69, 27)
+        Me.btnCancel.TabIndex = 3
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'lblSelect
+        '
+        Me.lblSelect.AutoSize = True
+        Me.lblSelect.Location = New System.Drawing.Point(1, 8)
+        Me.lblSelect.Name = "lblSelect"
+        Me.lblSelect.Size = New System.Drawing.Size(139, 13)
+        Me.lblSelect.TabIndex = 4
+        Me.lblSelect.Text = "Select the forms to activate:"
         '
         'frmDataForms
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(697, 385)
-        Me.Controls.Add(Me.cmdCancel)
-        Me.Controls.Add(Me.cmdHelp)
-        Me.Controls.Add(Me.cmdApply)
-        Me.Controls.Add(Me.lstvForms)
+        Me.ClientSize = New System.Drawing.Size(561, 301)
+        Me.Controls.Add(Me.lblSelect)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnHelp)
+        Me.Controls.Add(Me.btnApply)
+        Me.Controls.Add(Me.lstViewForms)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Name = "frmDataForms"
-        Me.Text = "Data Forms"
+        Me.Text = "Data Key Entry Forms"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents cmdApply As System.Windows.Forms.Button
-    Friend WithEvents cmdHelp As System.Windows.Forms.Button
-    Friend WithEvents cmdCancel As System.Windows.Forms.Button
-    Friend WithEvents lstvForms As System.Windows.Forms.ListView
+    Friend WithEvents btnApply As System.Windows.Forms.Button
+    Friend WithEvents btnHelp As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents lstViewForms As System.Windows.Forms.ListView
+    Friend WithEvents formName As ColumnHeader
+    Friend WithEvents formDescription As ColumnHeader
+    Friend WithEvents lblSelect As Label
 End Class
