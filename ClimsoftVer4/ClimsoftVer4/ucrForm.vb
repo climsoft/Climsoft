01dc0fc19f7e653dc989bfde8f7f3347f1b8f34a
patowhiz <conlooptechnologies@gmail.com>
2020-01-16 13:19:41 +0300

    new controls

---
+++ ClimsoftVer4/ClimsoftVer4/ucrForm.vb
@@ -0,0 +1,5 @@
+﻿Public Class ucrForm
+
+
+
+End Class
---
+++ ClimsoftVer4/ClimsoftVer4/ucrTextBoxNew.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ucrTextBoxNew
+    Inherits ClimsoftVer4.ucrValue
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtBox
+        '
+        Me.txtBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtBox.Location = New System.Drawing.Point(0, 0)
+        Me.txtBox.Name = "txtBox"
+        Me.txtBox.Size = New System.Drawing.Size(51, 20)
+        Me.txtBox.TabIndex = 579
+        '
+        'ucrTextBoxNew
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.txtBox)
+        Me.Name = "ucrTextBoxNew"
+        Me.Size = New System.Drawing.Size(51, 20)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtBox As TextBox
+End Class
