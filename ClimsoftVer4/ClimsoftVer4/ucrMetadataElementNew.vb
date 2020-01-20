44284047c7e58b5e028435772772ff617d5c7a00
Lazarus Kioko Muthenya <muthenyakioko@gmail.com>
2020-01-20 14:02:51 +0300

    Code

--- ClimsoftVer4/ClimsoftVer4/ucrMetadataElementNew.vb
+++ ClimsoftVer4/ClimsoftVer4/ucrMetadataElementNew.vb
@@ -1,3 +1,9 @@
 ﻿Public Class ucrMetadataElementNew
+    Private Sub ucrMetadataElementNew_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If bFirstLoad Then
 
+            SetUpButtonAddNew(btnAddNew)
+            SetUpButtonCancel(btnCancel)
+        End If
+    End Sub
 End Class
