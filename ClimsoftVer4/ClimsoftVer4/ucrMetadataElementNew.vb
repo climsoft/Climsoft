0288f2b0378f347a0aa13757ad746395fa2e323e
Lazarus Kioko Muthenya <muthenyakioko@gmail.com>
2020-01-20 14:16:54 +0300

    Code

--- ClimsoftVer4/ClimsoftVer4/ucrMetadataElementNew.vb
+++ ClimsoftVer4/ClimsoftVer4/ucrMetadataElementNew.vb
@@ -9,6 +9,19 @@
             SetUpButtonClear(btnClear)
             SetUpButtonView(btnView)
 
+            'todo pass in the data identifier here in place of nothing.
+            'This will be used by the data definition
+            SetDataIdentifier(Nothing)
+
+
         End If
+    End Sub
+
+    Public Sub SetValueControlProperties()
+        For Each ctr As ucrValue In lstUcrValues
+            'set the properties that will be used as part of the data definition
+            'todo. set up the properties of the child controls and the definition of the data structure
+            'the properties will be the field name...
+        Next
     End Sub
 End Class
