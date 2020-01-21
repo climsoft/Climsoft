6cb48301659c43b39210c1eba931aa66c7f9d571
Wycklife Bonyo <bonyowycklife@gmail.com>
2020-01-21 11:11:46 +0300

    More addition

--- ClimsoftVer4/ClimsoftVer4/ucrMetadataScheduleClassNew.Designer.vb
+++ ClimsoftVer4/ClimsoftVer4/ucrMetadataScheduleClassNew.Designer.vb
@@ -29,6 +29,14 @@ Partial Class ucrMetadataScheduleClassNew
         Me.ucrStationSelector = New ClimsoftVer4.ucrComboboxNew()
         Me.lblDescription = New System.Windows.Forms.Label()
         Me.ucrTextBoxNewDescription = New ClimsoftVer4.ucrTextBoxNew()
+        Me.GroupBox13 = New System.Windows.Forms.GroupBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnView = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnAddNew = New System.Windows.Forms.Button()
+        Me.GroupBox13.SuspendLayout()
         Me.SuspendLayout()
         '
         'lblScheduleClass
@@ -98,10 +106,87 @@ Partial Class ucrMetadataScheduleClassNew
         Me.ucrTextBoxNewDescription.TabIndex = 7
         Me.ucrTextBoxNewDescription.TextboxValue = ""
         '
+        'GroupBox13
+        '
+        Me.GroupBox13.Controls.Add(Me.btnClear)
+        Me.GroupBox13.Controls.Add(Me.btnView)
+        Me.GroupBox13.Controls.Add(Me.btnDelete)
+        Me.GroupBox13.Controls.Add(Me.btnUpdate)
+        Me.GroupBox13.Controls.Add(Me.btnSave)
+        Me.GroupBox13.Controls.Add(Me.btnAddNew)
+        Me.GroupBox13.Location = New System.Drawing.Point(8, 341)
+        Me.GroupBox13.Margin = New System.Windows.Forms.Padding(4)
+        Me.GroupBox13.Name = "GroupBox13"
+        Me.GroupBox13.Padding = New System.Windows.Forms.Padding(4)
+        Me.GroupBox13.Size = New System.Drawing.Size(819, 42)
+        Me.GroupBox13.TabIndex = 8
+        Me.GroupBox13.TabStop = False
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(549, 6)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(108, 33)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnView
+        '
+        Me.btnView.Location = New System.Drawing.Point(680, 6)
+        Me.btnView.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnView.Name = "btnView"
+        Me.btnView.Size = New System.Drawing.Size(108, 33)
+        Me.btnView.TabIndex = 5
+        Me.btnView.Text = "View"
+        Me.btnView.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(419, 6)
+        Me.btnDelete.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(108, 33)
+        Me.btnDelete.TabIndex = 3
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(288, 6)
+        Me.btnUpdate.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(108, 33)
+        Me.btnUpdate.TabIndex = 2
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(157, 6)
+        Me.btnSave.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(108, 33)
+        Me.btnSave.TabIndex = 1
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnAddNew
+        '
+        Me.btnAddNew.Location = New System.Drawing.Point(27, 6)
+        Me.btnAddNew.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnAddNew.Name = "btnAddNew"
+        Me.btnAddNew.Size = New System.Drawing.Size(108, 33)
+        Me.btnAddNew.TabIndex = 0
+        Me.btnAddNew.Text = "AddNew"
+        Me.btnAddNew.UseVisualStyleBackColor = True
+        '
         'ucrMetadataScheduleClassNew
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.GroupBox13)
         Me.Controls.Add(Me.ucrTextBoxNewDescription)
         Me.Controls.Add(Me.lblDescription)
         Me.Controls.Add(Me.ucrStationSelector)
@@ -112,6 +197,7 @@ Partial Class ucrMetadataScheduleClassNew
         Me.Margin = New System.Windows.Forms.Padding(5, 5, 5, 5)
         Me.Name = "ucrMetadataScheduleClassNew"
         Me.Size = New System.Drawing.Size(840, 430)
+        Me.GroupBox13.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -124,4 +210,11 @@ Partial Class ucrMetadataScheduleClassNew
     Friend WithEvents ucrStationSelector As ucrComboboxNew
     Friend WithEvents lblDescription As Label
     Friend WithEvents ucrTextBoxNewDescription As ucrTextBoxNew
+    Friend WithEvents GroupBox13 As GroupBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnView As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnAddNew As Button
 End Class
