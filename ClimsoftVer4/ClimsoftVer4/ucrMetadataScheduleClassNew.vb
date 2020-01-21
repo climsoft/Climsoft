976367dfc207e72e0565a72c5b2e7278838f9da5
Wycklife Bonyo <bonyowycklife@gmail.com>
2020-01-21 12:19:08 +0300

    schedule class code

--- ClimsoftVer4/ClimsoftVer4/ucrMetadataScheduleClassNew.vb
+++ ClimsoftVer4/ClimsoftVer4/ucrMetadataScheduleClassNew.vb
@@ -1,3 +1,22 @@
 ﻿Public Class ucrMetadataScheduleClassNew
+    Private Sub ucrMetadataScheduleClassNew_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If bFirstLoad Then
 
+            SetUpButtonAddNew(btnAddNew)
+            SetUpButtonSave(btnSave)
+            SetUpButtonUpdate(btnUpdate)
+            SetUpButtonDelete(btnDelete)
+            SetUpButtonClear(btnClear)
+            SetUpButtonView(btnView)
+
+            'todo pass in the data identifier here in place of nothing.
+            'This will be used by the data definition
+            SetDataIdentifier(Nothing)
+            SetValueControlProperties()
+            SetUpValueControls()
+            InitialiseDatastructure()
+
+            bFirstLoad = False
+        End If
+    End Sub
 End Class
